551be5bd95459a263cde110bddc7d1eaa04a5e20
Shubh-Arora05 <shubharoraofficial05@gmail.com>
2025-02-15 17:12:50 +0530

    add node module

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-// require('dotenv').config() ;
+
 const jwt_secret = "123";
 const authorization = async (req, res, next) => {
   const authheader = await req.headers.authorization;
